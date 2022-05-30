113fbbdf1bf84df8eaab667ff148ccc701da03ae
Com-Red <98872839+Com-Red@users.noreply.github.com>
2022-05-30 16:38:08 +0500

    123

---
+++ src/components/menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Menu extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <h1>123</h1>
+        )
+    }
+}
+
+export default (Menu);
