4e6b019c98997a5aec08bf08ffd27221b1416ceb
Com-Red <98872839+Com-Red@users.noreply.github.com>
2022-06-01 20:34:19 +0500

    123

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Menu from './components/menu/menu';
+import {Routes, Route, Link} from 'react-router-dom';
+import Page1 from './components/page/page1';
+import Page2 from './components/page/page2';
+import Page3 from './components/page/page3';
+import Test from './components/page/test';
+import Footer from './components/page/footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+      <Routes>
+        <Route path='/' element={<Page1/>}/>
+        <Route path='Page2' element={<Page2/>}/>
+        <Route path='Page3' element={<Page3/>}/>
+        <Route path='Test' element={<Test/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import './menu.css';
+import Test from "../page/test";
+import {NavLink, Outlet} from 'react-router-dom'
+
+class Menu extends React.Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div className="menu">
+                <img src="../data/image/kub.png" alt=""></img>
+                <ul>
+                    <NavLink to="/"><li>Главная</li></NavLink>
+                    <NavLink to="Page2"><li>Page2</li></NavLink>
+                    <NavLink to="Page3"><li>Page3</li></NavLink>
+                    <NavLink to="Test"><li>Test</li></NavLink>
+                    <li>Классы
+                        <ul>
+                            <li id='pageclass11'>11 класс</li>
+                            <li id='pageclass10'>10 класс</li>
+                            <li id='pageclass9'>9 классы</li>
+                        </ul>
+                        </li>
+                        <li>Blog
+                        <ul>
+                            <li>123</li>
+                            <li>321</li>
+                        </ul>
+                    </li>
+                    <li>Contact</li>
+                </ul>
+                <div className="menu1"></div>
+            </div>
+        )
+    }
+}
+
+export default (Menu);
