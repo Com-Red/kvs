eb82173a827b858cc12dc56dfd6f875d5976f8f4
Com-Red <98872839+Com-Red@users.noreply.github.com>
2022-06-01 10:08:13 +0500

    123

--- src/components/page/test.jsx
+++ src/components/page/test.jsx
@@ -6,8 +6,11 @@ const active = 13;
 const mass = {
     name: 'Tom',
     age: 33,
-    obj: [1,2,3,4,5,6]
+    obj: [1,2,3,4,5,6],
+    cif: 0,
 }
+
+
 
 const obj = [
     [{name: 'Bob'}]
@@ -15,16 +18,25 @@ const obj = [
 
 let mass2 = mass.obj.map((i) => <p>{i}</p>);
 
-function test1(){
-    
-}
+
 
 class Test extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            counter: 0
+        };
+    }
+
+    handlerClick(){
+        this.setState({counter: this.state.counter + 1});
+    }
+
     render(){
         return(
             <>
                 <h1>{Data.data[0][0].name}</h1>
-                {}
+                <button id='123' onClick={this.handlerClick}>Push {this.state.counter}</button>
             </>
         );
     }
